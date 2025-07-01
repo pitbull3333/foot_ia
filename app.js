89192d7bb05d940aae093210543aba4462c143d2
unknown <xthieuleuxjeux@gmail.com>
2025-07-02 01:52:53 +0200

    jouer v1

--- app.js
+++ app.js
@@ -33,14 +33,30 @@ const server = app.listen(port,(err)=>{
   }
 });
 const io = new Server(server);
+
+
+let model_loaded = false;
+let loadedModel = null;
+
+
 // Créer les différente réponce au action demmander par les client
 app.get("/",(req,res)=>{
 	res.render("index.html",{});
 })
+app.post("/entrainee",(req,res)=>{
+  modeEntrainement = true;
+  horloge();
+  res.send("Entraînement en cours");
+})
+app.post("/jouer",(req,res)=>{
+  modeEntrainement = false;
+  horloge();
+  res.send("Jeu en cours");
+})
 // Crée le réseau de neurones
 function createModel(){
   const model = tf.sequential();
-  model.add(tf.layers.dense({inputShape:[2],units:32,activation:"relu"}));// inputShape 2 entrée, units 32 neurone pour la premiaire couche
+  model.add(tf.layers.dense({inputShape:[4],units:32,activation:"relu"}));// inputShape 2 entrée, units 32 neurone pour la premiaire couche
   model.add(tf.layers.dense({units:32,activation:"relu"}));// units 32 neurone pour la deuxieme couche
   model.add(tf.layers.dense({units:actions.length,activation:"linear"}));// units actions.length actions possibles
   model.compile({ optimizer:"adam",loss:"meanSquaredError"});
@@ -64,6 +80,7 @@ io.on("connection",(socket) => {
   let dataUrl = canvas.toDataURL();
   socket.emit("image",dataUrl);
 });
+//let model_loaded = false;
 // Fonction dessin
 function dessin(){
   ctx.clearRect(0,0,largeur_terrain,longueur_terrain);
@@ -143,7 +160,7 @@ function getReward(x, y) {
 const model = createModel();
 let explorationRate = 1;
 const explorationDecay = 0.9997;
-const explorationMin = 0.8;//0.5
+const explorationMin = 0.9;//0.5
 const discountFactor = 0.8;
 // Choisir une action (exploration ou exploitation)
 function chooseAction(state) {
@@ -158,86 +175,103 @@ function chooseAction(state) {
     return actionIndex;
   }
 }
-
-let modelSaved = false;
-let intervalId = setInterval(async () => {
+// Entraînement du model
+const entrainement = async () => {
+  console.log(explorationRate);
   // État courant (normalisé)
-  const state = [xr1 / largeur_terrain, yr1 / longueur_terrain];
-
+  const state = [xr1 / largeur_terrain, yr1 / longueur_terrain,objectifX / largeur_terrain,objectifY / longueur_terrain];
   // Choix de l'action
   const actionIndex = chooseAction(state);
   const action = actions[actionIndex];
-
   // Appliquer l'action
   direction_r1_deg = actions[actionIndex];
   vitesse_r1 = 1;
-
   // Déplacement
   const direction_r1_rad = direction_r1_deg * (Math.PI / 180);
   const coeffX = Math.cos(direction_r1_rad);
   const coeffY = Math.sin(direction_r1_rad);
   xr1 -= vitesse_r1 * coeffX;
   yr1 += vitesse_r1 * coeffY;
-
   // Empêche de sortir du terrain
   //xr1 = Math.max(rayon_robot, Math.min(largeur_terrain - rayon_robot, xr1));
   //yr1 = Math.max(rayon_robot, Math.min(longueur_terrain - rayon_robot, yr1));
-
   // Nouvel état et récompense
-  const newState = [xr1 / largeur_terrain, yr1 / longueur_terrain];
+  const newState = [xr1 / largeur_terrain, yr1 / longueur_terrain,objectifX / largeur_terrain,objectifY / longueur_terrain];
   const reward = getReward(xr1, yr1);
-
   // Prédiction Q-values
   const oldQ = model.predict(tf.tensor2d([state]));
   const newQ = model.predict(tf.tensor2d([newState]));
-
   const oldQValues = oldQ.dataSync();
   const newQValues = newQ.dataSync();
-
   const targetQ = [...oldQValues];
   const bestFutureQ = Math.max(...newQValues);
   targetQ[actionIndex] = reward + discountFactor * bestFutureQ;
-
   await model.fit(
     tf.tensor2d([state]),
     tf.tensor2d([targetQ]),
     { epochs: 1, verbose: 0 }
   );
-
   oldQ.dispose();
   newQ.dispose();
-
   // Mise à jour exploration
   if (explorationRate > explorationMin) {
     explorationRate *= explorationDecay;// diminue progressivement explorationRate
-  }else if (!modelSaved) {// Sauvegarde du modèle une seule fois
-    //await model.save('file://ia/mon_modele');
-    //await model.save('file://ia/mon_modele');
-    //console.log("Modèle sauvegardé");
-    // Sauvegarde manuelle
-    const saveModel = async (model) => {
-      const saveResult = await model.save(tf.io.withSaveHandler(async (artifacts) => {
-      fs.mkdirSync('./ia', { recursive: true });
-      fs.writeFileSync('./ia/mon_modele.json', JSON.stringify(artifacts.modelTopology));
-      fs.writeFileSync('./ia/mon_modele.weights.bin', Buffer.from(artifacts.weightData));
-      return { modelArtifactsInfo: { dateSaved: new Date(), modelTopologyType: 'JSON', weightDataBytes: artifacts.weightData.byteLength } };
-    }))};
-    await saveModel(model);
-    modelSaved = true;
-    clearInterval(intervalId); // Stoppe la boucle d'entraînement
-    console.log("Entraînement terminé");
   }
-  console.log(explorationRate);
   // Affichage
   dessin();
+}
+let intervalId = null;
+function horloge(){
+  if(intervalId !== null) return; // ne lance pas si déjà en cours
+  explorationRate = 1;         // remise à 1 pour un nouvel entraînement
+  xr1 = largeur_terrain / 2;
+  yr1 = longueur_terrain / 2;
+  direction_r1_deg = 135;
+  lastDistance = null;
+  intervalId = setInterval(async () => {
+    if(modeEntrainement == true){
+      await entrainement();
+      if(explorationRate <= explorationMin){
+        // Sauvegarde du model et arrer du setInterval
+        const saveModel = async (model) => {
+          const saveResult = await model.save(tf.io.withSaveHandler(async (artifacts) => {
+            fs.mkdirSync('./ia', { recursive: true });
+            fs.writeFileSync('./ia/mon_modele.json', JSON.stringify(artifacts.modelTopology));
+            fs.writeFileSync('./ia/mon_modele.weights.bin', Buffer.from(artifacts.weightData));
+            return { modelArtifactsInfo: { dateSaved: new Date(), modelTopologyType: 'JSON', weightDataBytes: artifacts.weightData.byteLength } };
+          }))
+        }
+        await saveModel(model);
+        clearInterval(intervalId);
+        intervalId = null;// remet à null pour permettre relancement
+        console.log("Entraînement terminé");
+      }
+    }else{
+      //console.log("jouer");
 
-  // Reset si l’objectif est atteint
-  const distance = Math.sqrt((xr1 - 900) ** 2 + (yr1 - 20) ** 2);
-  if (distance < 10) {
-    xr1 = largeur_terrain / 2;
-    yr1 = longueur_terrain / 2;
-    vitesse_r1 = 0;
-    direction_r1_deg = 135;
-    console.log("Objectif atteint ! Reset position.");
-  }
-},20);
+
+      if (!model_loaded) {
+        loadedModel = await tf.loadLayersModel('file://./ia/valider/mon_modele.json');
+        model_loaded = true;
+      }
+
+
+
+      const state = [xr1 / largeur_terrain, yr1 / longueur_terrain, objectifX / largeur_terrain, objectifY / longueur_terrain];
+      const inputTensor = tf.tensor2d([state]);
+      //const prediction = model.predict(inputTensor);
+      const prediction = loadedModel.predict(inputTensor);
+      const actionIndex = prediction.argMax(1).dataSync()[0];
+      inputTensor.dispose();
+      prediction.dispose();
+      direction_r1_deg = actions[actionIndex];
+      vitesse_r1 = 1;
+      const direction_r1_rad = direction_r1_deg * (Math.PI / 180);
+      const coeffX = Math.cos(direction_r1_rad);
+      const coeffY = Math.sin(direction_r1_rad);
+      xr1 -= vitesse_r1 * coeffX;
+      yr1 += vitesse_r1 * coeffY;
+      dessin();
+    }
+  },20);
+}
--- back/app.js
+++ back/app.js
@@ -5,6 +5,7 @@ const {
   Server,
   createCanvas,
   cors,
+  tf,
 } = require('./js/dependence');
 const {
 	port,
@@ -36,42 +37,231 @@ const io = new Server(server);
 app.get("/",(req,res)=>{
 	res.render("index.html",{});
 })
-//
-const taille_map = 600;
-let x = 0;
-let vitesse = 2;
-let x_direction = "+";
-const canvas = createCanvas(taille_map,taille_map);
+app.post("/entrainee",(req,res)=>{
+  modeEntrainement = true;
+  horloge();
+  res.send("Entraînement en cours");
+})
+app.post("/jouer",(req,res)=>{
+  modeEntrainement = false;
+  horloge();
+  res.send("Jeu en cours");
+})
+// Crée le réseau de neurones
+function createModel(){
+  const model = tf.sequential();
+  model.add(tf.layers.dense({inputShape:[4],units:32,activation:"relu"}));// inputShape 2 entrée, units 32 neurone pour la premiaire couche
+  model.add(tf.layers.dense({units:32,activation:"relu"}));// units 32 neurone pour la deuxieme couche
+  model.add(tf.layers.dense({units:actions.length,activation:"linear"}));// units actions.length actions possibles
+  model.compile({ optimizer:"adam",loss:"meanSquaredError"});
+  return model;
+}
+// Définir les actions possibles
+const actions = [0,45,90,135,180,225,270,315];
+// inissialisation de variables
+const longueur_terrain = 1200;
+const largeur_terrain = longueur_terrain / 1.75;
+const rayon_robot = largeur_terrain / 35;
+let vitesse = 0;
+let xr1 = largeur_terrain / 2;
+let yr1 = longueur_terrain / 2;
+let direction_r1_deg = 135;// 0 = horizontal gauche en sens inverce des éguille d'une montre
+const canvas = createCanvas(largeur_terrain,longueur_terrain);
 const ctx = canvas.getContext("2d",{willReadFrequently:true});
-ctx.clearRect(0,0,taille_map,taille_map);
-  ctx.fillStyle = "#ff0000";
-  ctx.fillRect(x,250,100,100);
-  dataUrl = canvas.toDataURL();
-  io.emit("image",dataUrl);
+dataUrl = canvas.toDataURL();
+io.emit("image",dataUrl);
 io.on("connection",(socket) => {
-  console.log("ok");
   let dataUrl = canvas.toDataURL();
   socket.emit("image",dataUrl);
 });
+let model_loaded = false;
+// Fonction dessin
 function dessin(){
-  let x_bis = x + 100;
-  if(x <= 0){
-    x += vitesse;
-    x_direction = "+";
-  } else if(x >= taille_map || x_bis >= taille_map){
-    x -= vitesse;
-    x_direction = "-";
-  } else {
-    if(x_direction == "+") x += vitesse;
-    else x -= vitesse;
+  ctx.clearRect(0,0,largeur_terrain,longueur_terrain);
+  // Dessine la ligne mediane
+  ctx.beginPath();
+  ctx.moveTo(0,longueur_terrain / 2);
+  ctx.lineTo(largeur_terrain,longueur_terrain / 2);
+  ctx.strokeStyle = "white";
+  ctx.lineWidth = 2;
+  ctx.stroke();
+  // Dessine le point au milieu du terin
+  ctx.beginPath();
+  ctx.fillStyle = "white";
+  ctx.arc(largeur_terrain / 2,longueur_terrain / 2,largeur_terrain / 70,0,Math.PI * 2);
+  ctx.fill();
+  // Dessine l'objectif
+  ctx.beginPath();
+  ctx.fillStyle = "white";
+  ctx.arc(objectifX,objectifY,largeur_terrain / 70,0,Math.PI * 2);
+  ctx.fill();
+  // Dessine la zone de réconpence
+  ctx.beginPath();
+  ctx.fillStyle = "white";
+  ctx.arc(objectifX,objectifY,(largeur_terrain / 70) + 50,0,Math.PI * 2);
+  ctx.stroke();
+  // Dessine les lignes de but
+  ctx.beginPath();
+  ctx.moveTo((largeur_terrain / 2) - 20,0);
+  ctx.lineTo((largeur_terrain / 2) + 20,0);
+  ctx.strokeStyle = "red";
+  ctx.lineWidth = 4;
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo((largeur_terrain / 2) - 20,longueur_terrain);
+  ctx.lineTo((largeur_terrain / 2) + 20,longueur_terrain);
+  ctx.strokeStyle = "blue";
+  ctx.lineWidth = 4;
+  ctx.stroke();
+  // Dessine le robot
+  ctx.beginPath();
+  ctx.fillStyle = "#f00";
+  ctx.arc(xr1,yr1,rayon_robot,0,Math.PI * 2);
+  ctx.fill();
+  const dataUrl = canvas.toDataURL();
+  io.emit("image",dataUrl);
+}
+// Créer une récompense
+const objectifX = largeur_terrain - (largeur_terrain / 4);
+const objectifY = longueur_terrain / 4;
+let lastDistance = null;
+function getReward(x, y) {
+  const distance = Math.sqrt((x - objectifX) ** 2 + (y - objectifY) ** 2);
+  let reward = 0;
+
+  // 1. Grosse récompense si proche de la cible
+  if (distance < 10) {
+    reward += 10;
   }
 
-  ctx.clearRect(0, 0, taille_map, taille_map);
-  ctx.fillStyle = "#ff0000";
-  ctx.fillRect(x, 250, 100, 100);
-  const dataUrl = canvas.toDataURL();
-  io.emit("image", dataUrl);
+  // 2. Récompense basée sur la réduction de la distance
+  if (lastDistance !== null) {
+    reward += lastDistance - distance; // positif si on se rapproche
+  }
+  lastDistance = distance;
+
+  // 3. Pénalité si on touche un mur
+  if (
+    x + rayon_robot >= largeur_terrain || x - rayon_robot <= 0 ||
+    y + rayon_robot >= longueur_terrain || y - rayon_robot <= 0
+  ) {
+    reward -= 5;
+  }
+
+  return reward;
 }
-let intervalId = setInterval(()=>{
+// Paramaitre d'aprentissage de l'IA
+const model = createModel();
+let explorationRate = 1;
+const explorationDecay = 0.9997;
+const explorationMin = 0.9;//0.5
+const discountFactor = 0.8;
+// Choisir une action (exploration ou exploitation)
+function chooseAction(state) {
+  if (Math.random() < explorationRate) {
+    return Math.floor(Math.random() * actions.length); // aléatoire
+  } else {
+    const inputTensor = tf.tensor2d([state]);
+    const prediction = model.predict(inputTensor);
+    const actionIndex = prediction.argMax(1).dataSync()[0];
+    inputTensor.dispose();
+    prediction.dispose();
+    return actionIndex;
+  }
+}
+// Entraînement du model
+const entrainement = async () => {
+  console.log(explorationRate);
+  // État courant (normalisé)
+  const state = [xr1 / largeur_terrain, yr1 / longueur_terrain,objectifX / largeur_terrain,objectifY / longueur_terrain];
+  // Choix de l'action
+  const actionIndex = chooseAction(state);
+  const action = actions[actionIndex];
+  // Appliquer l'action
+  direction_r1_deg = actions[actionIndex];
+  vitesse_r1 = 1;
+  // Déplacement
+  const direction_r1_rad = direction_r1_deg * (Math.PI / 180);
+  const coeffX = Math.cos(direction_r1_rad);
+  const coeffY = Math.sin(direction_r1_rad);
+  xr1 -= vitesse_r1 * coeffX;
+  yr1 += vitesse_r1 * coeffY;
+  // Empêche de sortir du terrain
+  //xr1 = Math.max(rayon_robot, Math.min(largeur_terrain - rayon_robot, xr1));
+  //yr1 = Math.max(rayon_robot, Math.min(longueur_terrain - rayon_robot, yr1));
+  // Nouvel état et récompense
+  const newState = [xr1 / largeur_terrain, yr1 / longueur_terrain,objectifX / largeur_terrain,objectifY / longueur_terrain];
+  const reward = getReward(xr1, yr1);
+  // Prédiction Q-values
+  const oldQ = model.predict(tf.tensor2d([state]));
+  const newQ = model.predict(tf.tensor2d([newState]));
+  const oldQValues = oldQ.dataSync();
+  const newQValues = newQ.dataSync();
+  const targetQ = [...oldQValues];
+  const bestFutureQ = Math.max(...newQValues);
+  targetQ[actionIndex] = reward + discountFactor * bestFutureQ;
+  await model.fit(
+    tf.tensor2d([state]),
+    tf.tensor2d([targetQ]),
+    { epochs: 1, verbose: 0 }
+  );
+  oldQ.dispose();
+  newQ.dispose();
+  // Mise à jour exploration
+  if (explorationRate > explorationMin) {
+    explorationRate *= explorationDecay;// diminue progressivement explorationRate
+  }
+  // Affichage
   dessin();
-},20);
+}
+let intervalId = null;
+function horloge(){
+  if(intervalId !== null) return; // ne lance pas si déjà en cours
+  explorationRate = 1;         // remise à 1 pour un nouvel entraînement
+  xr1 = largeur_terrain / 2;
+  yr1 = longueur_terrain / 2;
+  direction_r1_deg = 135;
+  lastDistance = null;
+  intervalId = setInterval(async () => {
+    if(modeEntrainement == true){
+      await entrainement();
+      if(explorationRate <= explorationMin){
+        // Sauvegarde du model et arrer du setInterval
+        const saveModel = async (model) => {
+          const saveResult = await model.save(tf.io.withSaveHandler(async (artifacts) => {
+            fs.mkdirSync('./ia', { recursive: true });
+            fs.writeFileSync('./ia/mon_modele.json', JSON.stringify(artifacts.modelTopology));
+            fs.writeFileSync('./ia/mon_modele.weights.bin', Buffer.from(artifacts.weightData));
+            return { modelArtifactsInfo: { dateSaved: new Date(), modelTopologyType: 'JSON', weightDataBytes: artifacts.weightData.byteLength } };
+          }))
+        }
+        await saveModel(model);
+        clearInterval(intervalId);
+        intervalId = null;// remet à null pour permettre relancement
+        console.log("Entraînement terminé");
+      }
+    }else{
+      //console.log("jouer");
+      if(!model_loaded){
+        //
+      }
+
+  const state = [xr1 / largeur_terrain, yr1 / longueur_terrain, objectifX / largeur_terrain, objectifY / longueur_terrain];
+  const inputTensor = tf.tensor2d([state]);
+  const prediction = model.predict(inputTensor);
+  const actionIndex = prediction.argMax(1).dataSync()[0];
+  inputTensor.dispose();
+  prediction.dispose();
+
+  direction_r1_deg = actions[actionIndex];
+  vitesse_r1 = 1;
+  const direction_r1_rad = direction_r1_deg * (Math.PI / 180);
+  const coeffX = Math.cos(direction_r1_rad);
+  const coeffY = Math.sin(direction_r1_rad);
+  xr1 -= vitesse_r1 * coeffX;
+  yr1 += vitesse_r1 * coeffY;
+
+  dessin();
+    }
+  },20);
+}
